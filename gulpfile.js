dd335e423a07826ee51be673a16c6c13fd1b7498
andy.profi <andy@profi-studio.ru>
2018-06-07 20:38:11 +0300

    Новый рабочий вариант с sass, babel, uglify, clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,66 @@
 //TODO кэширование, чтобы пересобирались не все файлы, а только измененные
+//TODO watch for files
+//TODO add CSS task
 var gulp = require('gulp');
 var rename = require("gulp-rename");
-//TODO sass
-//var minifyCSS = require('gulp-csso');
-var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps'); //TODO промежуточный sourcemap на sass, а не на css после обработки sass()
 
-gulp.task('css', function(){
-	return gulp.src(['./www/local/templates/*/css/**/*.scss',
-	                 './www/local/templates/*/components/**/*.scss',
-	], {base: '.'})
-		//.pipe(less())
-		//.pipe(minifyCSS())
+var base = 'www';
+var paths = {
+	styles: {
+		src: ['./www/local/templates/*/css/**/*.scss',
+		      './www/local/templates/*/components/**/*.scss'
+		],
+		dest: './' + base + '/'
+	},
+	scripts: {
+		src: ['./www/local/templates/*/js/**/*[^.min].js',
+		      './www/local/templates/*/components/**/*[^.min].js'
+		],
+		dest: './' + base + '/'
+	}
+};
+
+function sassStyles() {
+	return gulp.src(paths.styles.src, {base: base})
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cleanCSS()) //TODO можно задать настройки по необходимым браузерам
 		.pipe(rename({
 			suffix: ".min",
 			extname: ".css"
 		}))
-		.pipe(gulp.dest('./'))
-});
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(paths.styles.dest))
+}
 
-gulp.task('js', function(){
-	return gulp.src(['./www/local/templates/*/js/**/*[^.min].js', //TODO not min
-		'./www/local/templates/*/components/**/*[^.min].js',
-	], {base: '.'})
-		.pipe(sourcemaps.init()) //TODO работает ли вообще?
+function scripts() {
+	return gulp.src(paths.scripts.src, {base: base})
+		.pipe(sourcemaps.init())
 		.pipe(rename({
 			suffix: ".min"
 		}))
-		//.pipe(concat('app.min.js'))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('./'))
-});
+		.pipe(babel())
+		.pipe(uglify())
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(paths.scripts.dest))
+}
 
-gulp.task('default', ['css', 'js']);
+function watch() {
+	gulp.watch(paths.scripts.src, scripts);
+	gulp.watch(paths.styles.src, sassStyles);
+}
+
+exports.sassStyles = sassStyles;
+exports.scripts = scripts;
+exports.watch = watch;
+
+//var build = gulp.series(clean, gulp.parallel(scss, js));
+var build = gulp.parallel(sassStyles, scripts);
+
+//gulp.task('default', ['scss', 'js']);
+gulp.task('default', build);
